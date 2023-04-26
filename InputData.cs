a10ac78f075ff5cae903edf8a63bdad300c24213
Marino <72434524+Osmanllari@users.noreply.github.com>
2023-04-26 12:47:04 +0200

    Add files via upload

---
+++ InputData.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Covid_Predictions.InputData
+{
+    public class InputData
+    {
+        [JsonPropertyName("iso_code")]
+        public string? IsoCode { get; set; }
+
+        [JsonPropertyName("location")]
+        public string? Location { get; set; }
+
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("population")]
+        public int Population { get; set; }
+    }
+
+}
